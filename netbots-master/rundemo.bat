fde88efac93e9cce7f92388b0b68d711f7077e30
KevinSongg <48466527+KevinSongg@users.noreply.github.com>
2020-05-29 19:33:22 -0700

    Add files via upload

---
+++ netbots-master/rundemo.bat
@@ -0,0 +1,11 @@
+CD /d "%~dp0"
+
+start "NetBot-Server" cmd /K py -3 src/netbots_server.py -p 20000 -stepsec 0.005 -games 100 -bots 5 -stepmax 3000
+
+start "hideincorner" cmd /K py -3 robots/hideincorner.py -p 20002 -sp 20000
+start "lighthouse" cmd /K py -3 robots/lighthouse.py -p 20003 -sp 20000
+start "scaredycat" cmd /K py -3 robots/scaredycat.py -p 20004 -sp 20000
+start "wallbanger" cmd /K py -3 robots/wallbanger.py -p 20006 -sp 20000
+start "kevinrobot" cmd /K py -3 robots/kevinrobot.py -p 20005 -sp 20000
+
+start "NetBot-Viewer" cmd /K py -3 src/netbots_viewer.py -p 20001 -sp 20000
